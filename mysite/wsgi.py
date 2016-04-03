27ec578516f1051248a4283c424192a794bb4e04
Viraj Navkal <vnavkal0@gmail.com>
2016-04-02 19:23:45 -0700

    add path to wsgi file

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
-
+import sys, os
+sys.path.insert(0, '/opt/python/current/app')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
